78b970476b9326eba2b25a8f86137e420bcaca23
HUNDZVL <nguyendoanquochung9a1@gmail.com>
2023-03-19 00:10:54 +0700

    Header#2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,26 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/33b47f946af7e325b048d8460c4032f1~c5_300x300.webp?x-expires=1679328000&x-signature=YnOEtVylgloiRYsycwDFwRughmc%3D"
+                alt="img"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('usernames')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
